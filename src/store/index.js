3077c46e530dbf2d1b4895f0526eb59a926dd19f
grinat <paladin2012gnu@gmail.com>
2017-12-20 19:37:22 +0300

    feat: import vuex data without vuex instanse

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,6 @@
  *
  * Created at     : 2017-12-19
  */
-import Vue from 'vue'
-import Vuex from 'vuex'
 import state from './state'
 import getters from './getters'
 import mutations from './mutations'
@@ -15,12 +13,10 @@ import actions from './actions'
 Vue.use(Vuex)
 
 export function createStore () {
-  return new Vuex.Store({
+  return {
     state,
     getters,
     actions,
-    mutations,
-    /* https://vuex.vuejs.org/en/strict.html */
-    strict: process.env.NODE_ENV !== 'production'
-  })
+    mutations
+  }
 }
